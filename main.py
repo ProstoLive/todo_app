b6b487875f4728075f4a1e9d570c71ea849a1fe6
ProstoLive <artem009saratov@gmail.com>
2024-04-27 14:03:26 +0400

    Added deletion func, improved JSON replies

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI, Path, Query, Depends, HTTPException, status
 from fastapi_users import FastAPIUsers
-from sqlalchemy import insert
+from sqlalchemy import insert, column
+from sqlalchemy.ext.asyncio import async_sessionmaker
+from sqlalchemy.orm import sessionmaker
 
 from auth.auth import auth_backend
-from auth.database import User, create_db_and_tables, engine
+from auth.database import User, engine
 from auth.manager import get_user_manager
 from auth.schemas import UserRead, UserCreate
 from models.models import tasks
@@ -31,9 +33,27 @@ app.include_router(
 current_user = fastapi_users.current_user()
 
 @app.post("/create_task")
-async def protected_route(name: str, description: str | None = None, User = Depends(current_user)):
+async def create_task(name: str, description: str | None = None, User = Depends(current_user)):
     conn = await engine.connect()
-    await conn.execute(insert(tasks).values(name=name, description=description, author_id = User.id))
+    id_new_task = await conn.execute(
+        insert(tasks).values(name=name, description=description, author_id = User.id).returning(tasks.c.id)
+    )
     await conn.commit()
-    return {"status": "ok"}
+    return {"status": "Created task",
+            "id": id_new_task.scalar()}
+
+@app.delete("/delete_task/{id}")
+async def delete_task(id: int, User = Depends(current_user)):
+    conn = await engine.connect()
+    res = await conn.execute(tasks.select().where((tasks.c.id == id) & (tasks.c.author_id == User.id)))
+    try:
+        res_obj = res.one()
+    except:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="No such task")
+    await conn.execute(tasks.delete().where(tasks.c.id == id))
+    await conn.commit()
+    return {"status": "Deleted task",
+            "task_id": id,
+            "author_id": User.id}
+
 
